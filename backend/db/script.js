77126025c80021d58e1e0f67d64cd597e7e6bc99
WPJengineer <jkelly20940@iesjoanramis.org>
2025-11-23 14:34:14 +0100

    factorise ajax searchbar in products page

--- backend/db/script.js
+++ backend/db/script.js
@@ -1,15 +1,12 @@
 const endPoint = '/student014/online_shop/backend/endpoints/product-search.php';
-const emptyEndPoint = '/student014/online_shop/backend/endpoints/empty-product-search.php';
 
 let str = "";
 showHint(str);
 
 function showHint(str) {
   if (str.length == null && str.length == 0) {
-      document.getElementById('txtHint').innerHTML = this.responseText;
-      xmlhttp.open('GET', emptyEndPoint, true);
-      xmlhttp.send(); 
-      return;
+    document.getElementById('txtHint').innerHTML = "";
+    return;
   } else {
       var xmlhttp = new XMLHttpRequest();
       xmlhttp.onreadystatechange = function() {
