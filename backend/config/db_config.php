7c9ceab6ceab9efb6e5bfc0d66771aa784765b18
WPJengineer <jkelly20940@iesjoanramis.org>
2025-12-10 15:47:31 +0100

    changed config file to remotehost.es

--- backend/config/db_config.php
+++ backend/config/db_config.php
@@ -1,14 +1,14 @@
 <?php
 
 // connect to database (remotehost.es)
-// $server_name = 'remotehost.es';
-// $user_name = 'dwess1234';
-// $password = 'Usertest1234.';
-// $db_name = 'dwesdatabase';
-// $conn = mysqli_connect($server_name, $user_name, $password, $db_name);
+$server_name = 'remotehost.es';
+$user_name = 'dwess1234';
+$password = 'Usertest1234.';
+$db_name = 'dwesdatabase';
+$conn = mysqli_connect($server_name, $user_name, $password, $db_name);
 
 // connect to database (local)
-$conn = mysqli_connect('localhost', 'root', '', 'online_shop_db', 3306);
+// $conn = mysqli_connect('localhost', 'root', '', 'online_shop_db', 3306);
 
 // check connection
 if (!$conn) {
