5fe9bfd10b0c3dfba5b0ab7ddfeeef91a6abe882
WPJengineer <jkelly20940@iesjoanramis.org>
2025-12-17 03:53:29 +0100

    started working on js for cart

---
+++ js/shopping_cart.js
@@ -0,0 +1,19 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+    // const btnHome = document.getElementById('btnHome');
+    // const btnHome2 = document.getElementById('btnHome2');
+    const btnLogo = document.getElementById('btnLogo');
+
+    // btnHome.addEventListener('click', () => {
+    //     window.location.href = "../index.html";
+    // });
+
+    // btnHome2.addEventListener('click', () => {
+    //     window.location.href = "../index.html";
+    // });
+
+    btnLogo.addEventListener('click', () => {
+        window.location.href = "../index.html";
+    });
+
+});
