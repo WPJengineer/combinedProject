396c36fb4c215269cb2fe111be72d7f29ec4afd0
WPJengineer <jkelly20940@iesjoanramis.org>
2026-01-09 19:25:37 +0100

    allowed secrets

--- backend/footer.php
+++ backend/footer.php
@@ -3,6 +3,25 @@
     <div>
         <button onclick="getLocation(); getLocalWeather();">click me</button>
     </div>
+    <!-- <script>
+        function getLocation() {
+            const apiKey = 'zpka_0c52a288c0d7420b9a9acf1bc51cf5d0_8a04fb91';
+            const options = {method: 'GET', headers: {Authorization: 'Bearer ' + apiKey}};
+            fetch('https://dataservice.accuweather.com/locations/v1/cities/search?q=maó', options)
+                .then(response => response.json())
+                .then(response => console.log(response))
+                .catch(err => console.error(err));
+        }
+
+        function getLocalWeather() {
+            const options = {method: 'GET', headers: {Authorization: 'Bearer ' + apiKey}};
+            fetch('https://dataservice.accuweather.com/currentconditions/v1/305482', options)
+                .then(response => response.json())
+                .then(response => console.log(response))
+                .catch(err => console.error(err));
+        }
+        
+    </script> -->
 </footer>
 <script src="/student014/shop/js/backendScript.js"></script>
 </body>
