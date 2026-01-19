c7df18c02f3ae67af7255997459ab1572b86b855
WPJengineer <jkelly20940@iesjoanramis.org>
2026-01-19 16:08:44 +0100

    class jan 19

--- backend/footer.php
+++ backend/footer.php
@@ -1,7 +1,7 @@
 <footer class="bg-light text-center p-6">
     <div class="copyright">&copy; 2025 TeamWear. All Rights Reserved.</div>
     <div>
-        <button onclick="getLocation(); getLocalWeather();">click me</button>
+        <button onclick="getLocalWeather();">click me</button>
     </div>
     <!-- <script>
         function getLocation() {
