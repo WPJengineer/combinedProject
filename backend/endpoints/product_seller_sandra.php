cea9695a4b59f318ea67ed2931ffa8453087fae7
WPJengineer <jkelly20940@iesjoanramis.org>
2026-01-22 21:15:10 +0100

    class work jan 22

---
+++ backend/endpoints/product_seller_sandra.php
@@ -0,0 +1,24 @@
+<?php
+
+header("Content-Type: application/json");
+
+$sql = "SELECT *
+FROM 014_products
+LIMIT 65
+;";
+
+include('../config/db_config.php');
+
+$result = mysqli_query($conn, $sql);
+
+$products = [];
+
+if (mysqli_num_rows($result) > 0) {
+  while ($row = mysqli_fetch_assoc($result)) {
+    $products[] = $row;
+  }
+}
+echo json_encode($products);
+mysqli_close($conn);
+exit;
+?>
--- backend/sandbox.php
+++ backend/sandbox.php
@@ -1,5 +1,24 @@
 <?php
-$url = 'https://dataservice.accuweather.com/currentconditions/v1/305482';
+// $url = 'https://dataservice.accuweather.com/currentconditions/v1/305482';
+// $ch = curl_init();
+// $data = array(
+// 	'Authorization' => 'Bearer zpka_0c52a288c0d7420b9a9acf1bc51cf5d0_8a04fb91'
+// );
+// $payload = json_encode($data);
+// curl_setopt($ch, CURLOPT_URL, 'https://dataservice.accuweather.com/currentconditions/v1/305482');
+// curl_setopt($ch, CURLOPT_POST, true);
+// curl_setopt($ch, CURLOPT_POSTFIELDS, $payload);
+// curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type:application/json'));
+// curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+// curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
+// // curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, FALSE);
+// $result = curl_exec($ch);
+// curl_close($ch);
+// echo $result;
+
+//  http://localhost/student022/sandbox/apiSandbox/endpointSendVendorJosh.php
+
+$url = 'https://remotehost.es/student022/sandbox/apiSandbox/endpointSendVendorJosh.php';
 $ch = curl_init();
 $data = array(
 	'Authorization' => 'Bearer zpka_0c52a288c0d7420b9a9acf1bc51cf5d0_8a04fb91'
@@ -15,4 +34,6 @@ curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
 $result = curl_exec($ch);
 curl_close($ch);
 echo $result;
+
+
 ?>
