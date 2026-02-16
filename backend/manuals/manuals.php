00499a1b5a20a11ea7bb32c90b987aae1a22a91c
WPJengineer <jkelly20940@iesjoanramis.org>
2026-02-16 13:01:20 +0100

    added user manual

--- backend/manuals/manuals.php
+++ backend/manuals/manuals.php
@@ -4,7 +4,7 @@
     <div class="flex flex-col items-center justify-center gap-6">
         <ul>
             <li class="button"><a href="/student014/shop/backend/manuals/installation.php">Installation Manual</a></li>
-            <li class="button"><a href="/student014/shop/backend/manuals/tecnical.php">Technical Manual</a></li>
+            <li class="button"><a href="/student014/shop/backend/manuals/technical.php">Technical Manual</a></li>
             <li class="button"><a href="/student014/shop/backend/manuals/user_manual.php">User Manual</a></li>
         </ul>
     </div>
--- backend/manuals/user_manual.php
+++ backend/manuals/user_manual.php
@@ -1,6 +1,6 @@
 <?php $backend = $_SERVER['DOCUMENT_ROOT'].'/student014/shop/backend/'; ?>
 <?php require($backend.'header.php'); ?>
 <main class="bg-green flex flex-col p-5 items-center justify-center" style="flex: 1;">
-    <p>Good luck...</p>
+    <a href="/student014/shop/backend/manuals/user_manual.pdf" class="text-4xl text-blue underline">User Manual</a>
 </main>
 <?php require($backend.'footer.php'); ?>
